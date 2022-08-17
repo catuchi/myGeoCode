b2aa390c4fe63f52ff4f55077ec68c85f77d6c21
Chibu <atuchibueze@gmail.com>
2022-08-17 10:16:58 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# MyGeoCode
+
+WebApp that uses Google geocoding API to get all location info including longitude and latitude from an input (address, city or state)
+
+## Tech Stack
+
+Javascript, HTML, CSS, Bootstrap, Axios
+
+## Version
+
+1.0.0
+
+## Documentation
+
+[Google Maps Platform](https://developers.google.com/maps/documentation/geocoding/start)
+
+## Run Locally
+
+Fork this repo
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install live server extension
+
+Start the server
--- main.js
+++ main.js
@@ -1,19 +1,75 @@
-// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCnRC7jU4wG8H6HUVlKzm6WoOAo77etdDI
-function geoCode() {
-  const location = "22 Main st Boston MA";
-  axios
-    .get(`https://maps.googleapis.com/maps/api/geocode/json`, {
-      params: {
-        address: location,
-        key: "AIzaSyCnRC7jU4wG8H6HUVlKzm6WoOAo77etdDI",
-      },
-    })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+// get location form
+const locationForm = document.getElementById("location-form");
 
-geoCode();
+// listen for event
+locationForm.addEventListener("submit", geoCode);
+
+function geoCode(e) {
+  e.preventDefault();
+
+  const location = document.getElementById("location-input").value;
+
+  if (location) {
+    axios
+      .get(`https://maps.googleapis.com/maps/api/geocode/json`, {
+        params: {
+          address: location,
+          key: "AIzaSyCnRC7jU4wG8H6HUVlKzm6WoOAo77etdDI",
+        },
+      })
+      .then((res) => {
+        // log full response
+        console.log(res);
+
+        // formatted address
+        const formattedAddress = res.data.results[0].formatted_address;
+
+        const formattedAddressOutput = `
+          <ul class="list-group">
+            <li class="list-group-item">${formattedAddress}</li>
+          </ul>
+        `;
+
+        const addressComponents = res.data.results[0].address_components;
+
+        let listGroup = document.createElement("ul");
+
+        listGroup.className = "list-group";
+
+        const addressComponentsOutput = addressComponents
+          .map(
+            (component) => `
+          <li class="list-group-item">
+            <strong>
+              ${component.types[0]}
+            </strong>: 
+            ${component.long_name}
+          </li>
+        `
+          )
+          .join("");
+
+        listGroup.innerHTML = addressComponentsOutput;
+
+        // Geometry
+        const { lat, lng } = res.data.results[0].geometry.location;
+        // console.log(lat, lng);
+
+        const geometryOutput = `
+          <ul class="list-group">
+            <li class="list-group-item"><strong>Latitude</strong>:${lat}</li>
+            <li class="list-group-item"><strong>Longitude</strong>:${lng}</li>
+          </ul>
+        `;
+
+        // Output to app
+        document.querySelector("#formatted-address").innerHTML =
+          formattedAddressOutput;
+        document.querySelector("#formatted-components").appendChild(listGroup);
+        document.querySelector("#geometry").innerHTML = geometryOutput;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
